b91ddc123f9b249fc0635ac1a7c91fec2777c076
Franisis <f.guzmana@uniandes.edu.co>
2024-03-09 18:14:04 -0500

    Done

--- Algoritmo.py
+++ Algoritmo.py
@@ -1,10 +1,19 @@
-def ordenamiento_por_insercion(lista):
-    for i in range(1, len(lista)):
-        actual = lista[i]
-        j = i - 1
-        while j >= 0 and lista[j] > actual:
-            lista[j + 1] = lista[j]
-            j -= 1
-        lista[j + 1] = actual
+def reorganizar_torres(torres):
+    n = len(torres)
+    cambios = True
+    movimientos = 0
+    while cambios:
+        cambios = False
+        for i in range(n - 1):
+            if torres[i] < torres[i + 1]:
+                torres[i] += 1
+                torres[i + 1] -= 1
+                cambios = True
+                movimientos += 1
+    return movimientos
 
 
+lista="36 38 14 7 7 7 2 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0".split(" ")
+lista = [int(i) for i in lista]
+print(reorganizar_torres(lista))
+print(lista)
